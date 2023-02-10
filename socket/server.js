1d95dcee3ffc22327e75c23d08acd15009fb6f97
Thomas <thomas.lefloch@eisti.fr>
2023-02-10 17:00:44 +0100

    feat: pushing data to MQTT

--- socket/server.js
+++ socket/server.js
@@ -1,44 +1,78 @@
-var SerialPort = require('serialport');
-var xbee_api = require('xbee-api');
+var SerialPort = require("serialport");
+var xbee_api = require("xbee-api");
 var C = xbee_api.constants;
 //var storage = require("./storage")
-require('dotenv').config()
+require("dotenv").config();
+var mqtt = require("mqtt");
+const weightSensorChannel = "0013a20012345678";
+const servoMotorChannel = "0013a20012345679";
+const client = mqtt.connect("mqtt://test.mosquitto.org");
 
+client.on("connect", function () {
+  client.subscribe(weightSensorChannel, (err) => {
+    if (err) {
+      console.error(`Can't subscribe to ${channel}`);
+      exit(-1);
+    }
+  });
+  client.subscribe(servoMotorChannel, (err) => {
+    if (err) {
+      console.error(`Can't subscribe to ${channel}`);
+      exit(-1);
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(topic, message.toString());
+});
 
 const SERIAL_PORT = process.env.SERIAL_PORT;
+const SAMPLING_RATE = process.env.IR_SAMPLING_RATE;
 
 var xbeeAPI = new xbee_api.XBeeAPI({
-  api_mode: 2
+  api_mode: 2,
 });
 
-let serialport = new SerialPort(SERIAL_PORT, {
-  baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
-}, function (err) {
-  if (err) {
-    return console.log('Error: ', err.message)
+let serialport = new SerialPort(
+  SERIAL_PORT,
+  {
+    baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
+  },
+  function (err) {
+    if (err) {
+      return console.log("Error: ", err.message);
+    }
   }
-});
+);
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
 serialport.on("open", function () {
-  var frame_obj = { // AT Request to be sent
+  var frame_obj = {
     type: C.FRAME_TYPE.AT_COMMAND,
     command: "NI",
-    commandParameter: [],
+    commandParameter: "LEFLOCH",
   };
 
   xbeeAPI.builder.write(frame_obj);
 
-  frame_obj = { // AT Request to be sent
+  var frame_obj = {
+    type: C.FRAME_TYPE.AT_COMMAND,
+    command: "IR",
+    commandParameter: SAMPLING_RATE.toString(16),
+  };
+
+  frame_obj = {
+    // AT Request to be sent
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
     destination64: "FFFFFFFFFFFFFFFF",
     command: "NI",
     commandParameter: [],
   };
   xbeeAPI.builder.write(frame_obj);
-
 });
 
 // All frames parsed by the XBee will be emitted here
@@ -46,7 +80,6 @@ serialport.on("open", function () {
 // storage.listSensors().then((sensors) => sensors.forEach((sensor) => console.log(sensor.data())))
 
 xbeeAPI.parser.on("data", function (frame) {
-
   //on new device is joined, register it
 
   //on packet received, dispatch event
@@ -55,26 +88,22 @@ xbeeAPI.parser.on("data", function (frame) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
     console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
-
   }
 
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
     //storage.registerSensor(frame.remote64)
-
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
+    client.publish("0013a20012345678", `${frame.analogSamples.AD1}`);
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX");
+    console.log(frame.analogSamples.AD1);
     //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
-
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    console.log("REMOTE_COMMAND_RESPONSE")
+    console.log("REMOTE_COMMAND_RESPONSE");
   } else {
     console.debug(frame);
-    let dataReceived = String.fromCharCode.apply(null, frame.commandData)
+    let dataReceived = String.fromCharCode.apply(null, frame.commandData);
     console.log(dataReceived);
   }
-
 });
