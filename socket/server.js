d069c97f47be43199a86403631d00cb6c625153b
B3r4ti0n <arcoucou77@gmail.com>
2023-06-02 09:59:52 +0200

    win led reset

--- socket/server.js
+++ socket/server.js
@@ -55,16 +55,44 @@ serialport.on("open", function () {
 client.on('connect', () => {
   client.subscribe('PlayerOne');
   client.subscribe('PlayerTwo');
+  client.subscribe('YesButton');
+  client.subscribe('NoButton');
+  client.subscribe('Complatible');
+  client.subscribe('NotComplatible');
 });
 
-client.on('message',(topic,message) => {
+client.on('message', (topic, message) => {
   switch (topic) {
     case 'PlayerOne':
       console.log(`TopicResponse: ${message.toString()}`);
-    break;
+      break;
     case 'PlayerTwo':
       console.log(`TopicResponse: ${message.toString()}`);
-    break;
+      break;
+    case 'YesButton':
+      console.log(`YesButton: ${message.toString()}`);
+      break;
+    case 'NoButton':
+      console.log(`NoButton: ${message.toString()}`);
+      break;
+    case 'Complatible':
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+        destination64: "FFFFFFFFFFFFFFFF",
+        command: "D3",
+        commandParameter: [0x05],
+      };
+      xbeeAPI.builder.write(frame_obj);
+      break;
+    case 'NotComplatible':
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+        destination64: "FFFFFFFFFFFFFFFF",
+        command: "D3",
+        commandParameter: [0x04],
+      };
+      xbeeAPI.builder.write(frame_obj);
+      break;
     default:
       break;
   }
@@ -73,35 +101,40 @@ client.on('message',(topic,message) => {
 xbeeAPI.parser.on("data", function (frame) {
 
   //console.log(frame);
-  if(C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type){
-      console.log(frame);
-      // Vérification des messages déjà publiés sur le topic "PlayerOne"
-      switch (frame.nodeIdentifier) {
-        case 'PlayerOne':
-          client.publish('PlayerOne', JSON.stringify(frame));
-          console.log(`PlayerOne Topic = ${JSON.stringify(frame)}`);
+  if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
+    console.log(frame);
+    // Vérification des messages déjà publiés sur le topic "PlayerOne"
+    switch (frame.nodeIdentifier) {
+      case 'PlayerOne':
+        client.publish('PlayerOne', JSON.stringify(frame));
+        console.log(`PlayerOne Topic = ${JSON.stringify(frame)}`);
         break;
-        case 'PlayerTwo':
-          client.publish('PlayerTwo', JSON.stringify(frame));
-          console.log(`PlayerTwo Topic = ${JSON.stringify(frame)}`);
+      case 'PlayerTwo':
+        client.publish('PlayerTwo', JSON.stringify(frame));
+        console.log(`PlayerTwo Topic = ${JSON.stringify(frame)}`);
         break;
-        default:
-          console.log(`xbee NI is not valide : ${frame.nodeIdentifier} \n PlayerOne or PlayerTwo awaited`);
+      case 'Validator':
+        client.publish('NotCompatible', JSON.stringify(frame));
+        console.log('Led reset');
         break;
-      }
+      default:
+        console.log(`xbee NI is not valide : ${frame.nodeIdentifier} \n PlayerOne or PlayerTwo awaited`);
+        break;
+    }
   }
 
   if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
     const buttonD0Pressed = frame.digitalSamples.DIO0 === 1;
     if (buttonD0Pressed) {
-      client.publish('YesButton',JSON.stringify(frame));
+      client.publish('YesButton', JSON.stringify(frame));
+      client.publish('Validation', JSON.stringify(frame));
       console.log('YesButton publish');
     }
 
     const buttonD1Pressed = frame.digitalSamples.DIO1 === 1;
     if (buttonD1Pressed) {
-      client.publish('NoButton',JSON.stringify(frame));
+      client.publish('NoButton', JSON.stringify(frame));
       console.log('NoButton publish');
     }
   }
